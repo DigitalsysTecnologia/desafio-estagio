4573157ff83dabcc1bb24653b277de04b07733f0
Andre Luis Alves Tavares <129228972+andret22@users.noreply.github.com>
2024-02-13 11:22:21 -0500

    Add files via upload

---
+++ foo/main.go
@@ -0,0 +1,36 @@
+package main
+
+import (
+	controllers "foo/Controllers"
+	dbconnections "foo/dboperations"
+	"foo/migration"
+
+	"github.com/gin-gonic/gin"
+)
+
+func init() {
+	//Tenta realizar conexão com o banco de dados
+	dbconnections.DBConnect()
+	//Inializa o banco de dados com GORM
+	migration.InitizalizeDB()
+}
+
+func main() {
+	r := gin.Default()
+
+	r.POST("/createchar", controllers.Autenticar, controllers.CriaPersonagem)
+	r.GET("/getallchar", controllers.Autenticar, controllers.RetornaTodos)
+	r.POST("/deletechar", controllers.Autenticar, controllers.DeletaPersonagem)
+	r.POST("/updatechar", controllers.Autenticar, controllers.AtualizaPersonagem)
+	r.POST("/findchar", controllers.Autenticar, controllers.BuscarPersonagem)
+	r.POST("/crateclass", controllers.Autenticar, controllers.CriaClasse)
+	r.POST("/crateguild", controllers.Autenticar, controllers.CriaGuilda)
+	r.POST("/createquest", controllers.Autenticar, controllers.CriaMissao)
+	r.POST("/givequest", controllers.Autenticar, controllers.AtribuirMissao)
+	r.POST("/quitquest", controllers.Autenticar, controllers.DeixaMissao)
+	r.POST("/joinguild", controllers.Autenticar, controllers.EntrarGuild)
+	r.POST("/leaveguild", controllers.Autenticar, controllers.DeixarGuild)
+	r.GET("/gettoken", controllers.SetToken)
+
+	r.Run() // listening on localhost:8080
+}
