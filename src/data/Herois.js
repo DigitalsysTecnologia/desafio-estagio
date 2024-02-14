0b343430fef1328a9f202660951583df955b93a7
natalia.massari <naty.massari25@gmail.com>
2024-02-14 18:50:15 -0300

    Limpeza

---
+++ src/data/Herois.js
@@ -0,0 +1,39 @@
+export const Herois = [
+  {
+    id: 1,
+    title: "Charlotte",
+    description: "Charlotte, a heroína arqueira, é uma mestra na arte do arco e flecha, defendendo os inocentes com precisão mortal.\nCom sua destreza incomparável, ela mira no coração da injustiça, lutando incansavelmente pela justiça e pela liberdade.",
+    power: 0.6,
+    health: 0.5,
+    stamina: 0.8,
+    img: require('../../assets/image/charlotte1.png'),
+  },
+  {
+    id: 2,
+    title: "Wizard",
+    description: "Wizard, o mago supremo, manipula os elementos com poder incomparável, tecendo magia para proteger os reinos da ameaça das trevas.\nCom seu cajado como foco, ele conjura feitiços ancestrais, iluminando o caminho da verdade e da sabedoria.",
+    power: 0.5,
+    health: 0.6,
+    stamina: 0.4,
+    img: require('../../assets/image/wizard.png'),
+    
+  },
+  {
+    id: 3,
+    title: "Vaulks",
+    description: "Vaulks, o valquírio destemido, cavalga pelos céus em busca de glória e honra, brandindo sua lança contra os inimigos dos deuses. \nCom sua armadura reluzente e asas poderosas, ele é a personificação da coragem e da justiça divina.",
+    power: 0.6,
+    health: 1,
+    stamina: 0.3,
+    img: require('../../assets/image/vaulks.png'),
+  },
+  {
+    id: 4,
+    title: "Lee",
+    description: "Lee, o ladrão sorrateiro, desliza pelas sombras com destreza inigualável, saqueando os mais impenetráveis tesouros com habilidade e astúcia.\nCom sua agilidade e perspicácia, ele desafia as defesas mais robustas, acumulando riquezas e fama na clandestinidade.",
+    power: 0.4,
+    health: 0.5,
+    stamina: 1,
+    img: require('../../assets/image/lee.png'),
+  },
+];
---
+++ src/data/Missoes.js
@@ -0,0 +1,40 @@
+
+export const Missoes = [
+    {
+      id: 1,
+      title: "Vovó precisa de ajuda!",
+      description:
+      "Você foi encarregado de encontrar os ingredientes para uma poção mágica que pode restaurar as memórias perdidas da vovó e irá enfrentar criaturas determinadas a impedir sua jornada em troca de uma torta de maçã.",
+      difficulty: 0.2,
+      img: require('../../assets/image/grandma.png'),
+    },
+    {
+      id: 2,
+      title: "O Desafio do Pescador",
+      description:
+      "Você é enviado a um lago misterioso para pescar um peixe lendário que pode conceder desejos. Enfrentando condições climáticas adversas e criaturas aquáticas hostis, você deve demonstrar habilidade e paciência para capturar o peixe e completar a missão.",
+      difficulty: 0.4,
+      img: require('../../assets/image/fish.png'),
+  
+      
+    },
+    {
+      id: 3,
+      title: "A Profundeza da Caverna",
+      description:
+      "Você é enviado para explorar uma caverna escura e perigosa em busca de um artefato lendário. Navegando por passagens estreitas e enfrentando criaturas subterrâneas hostis, você deve usar sua astúcia e habilidades para superar os desafios e alcançar o artefato antes que seja tarde demais.",
+      difficulty: 0.6,
+      img: require('../../assets/image/cave.png'),
+  
+    },
+    {
+      id: 4,
+      title: "A Prova da Caverna",
+      description:
+        "Você é convocado para derrotar o temível chefão que reside nas profundezas de uma caverna sinistra. Enfrente seus lacaios e desafios mortais enquanto busca o confronto final. Com coragem e estratégia, você deve superar o chefão e garantir a segurança da região.",
+        difficulty: 0.9,
+        img: require('../../assets/image/boss.png'),
+  
+    },
+  ];
+  
