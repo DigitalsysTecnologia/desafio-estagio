d1d83ff9cd35243b64ca1946fa1116e1059b3dce
Bruno Brandão Silva <brunowbrandao@gmail.com>
2024-02-15 23:36:21 -0300

    adds Loading component and related styles

---
+++ src/components/Loading.tsx
@@ -0,0 +1,14 @@
+import '../styles/Loading.css';
+
+function Loading() {
+    return <div className="LoaderContainer">
+        <div className="Loader">
+            <div className="Outer"></div>
+            <div className="Middle"></div>
+            <div className="Inner"></div>
+        </div>
+    </div>
+
+}
+
+export default Loading;
