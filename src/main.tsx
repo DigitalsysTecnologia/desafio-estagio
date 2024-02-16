1df5659a191735d114f187b82a2bd17e684b2d01
Bruno Brandão Silva <brunowbrandao@gmail.com>
2024-02-15 23:34:33 -0300

    Added react-router-dom dependency and route updates

--- src/main.tsx
+++ src/main.tsx
@@ -1,10 +1,41 @@
 import React from 'react'
 import ReactDOM from 'react-dom/client'
-import App from './App.tsx'
-import './index.css'
+import Index from './routes/Index'
+import Character from './routes/Character'
+import './styles/main.css'
+import {
+  createBrowserRouter,
+  RouterProvider,
+} from "react-router-dom";
+import Characters from './routes/Characters'
+import Adventures from './routes/Adventures'
+import ErroHandler from './routes/ErrorHandler'
+
+const router = createBrowserRouter([
+  {
+    path: "/",
+    element: <Index />,
+  },
+  {
+    path: "/characters",
+    element: <Characters />,
+  },
+  {
+    path: "/character/:id",
+    element: <Character />,
+  },
+  {
+    path: "/adventures",
+    element: <Adventures />,
+  },
+  {
+    path: "*",
+    element: <ErroHandler />,
+  }
+]);
 
 ReactDOM.createRoot(document.getElementById('root')!).render(
   <React.StrictMode>
-    <App />
+    <RouterProvider router={router} />
   </React.StrictMode>,
 )
